99eaa84e52c3a4096dfb0c4124d21acf0bfa60fe
Sean Harrell <seanvharrell@gmail.com>
2020-03-26 06:21:19 +0000

    added empty src folder with index.js file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./app');
 // write your code here
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8484;
 
 server.listen(PORT,() => {
   console.log(`Server is listening on http://localhost:${PORT}`);
